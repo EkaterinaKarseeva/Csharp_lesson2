89274eba20db36d22d611b8cb9252a9e2a4a1e1d
EkaterinaKarseeva <154433790+EkaterinaKarseeva@users.noreply.github.com>
2023-12-28 07:13:13 +0400

    1 задача

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// 1 Задача
+
+Console.WriteLine("Введите число для проверки:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number % 7 == 0 && number % 23 == 0)
+            {
+                Console.WriteLine($"{number} кратно одновременно 7 и 23");
+            }
+            else
+            {
+                Console.WriteLine($"{number} не кратно одновременно 7 и 23");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+        }
